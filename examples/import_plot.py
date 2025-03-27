acd911d032fa91cdda8f92c5905ba0675a0f1b92
Finwp <finnwp@me.com>
2025-03-27 10:49:39 +0100

    adding wk8 example file

---
+++ examples/import_plot.py
@@ -0,0 +1,12 @@
+# wk 8 importing and plotting homework
+
+# there aare 4 types of files 
+# - IEA-15-240-RWT_AeroDyn15_blade.dat
+# - IEA_15MW_RWT_Onshore.opt
+# - Airfoils/IEA-15-240-RWT_AF00_Coords.txt
+# - IEA-15-240-RWT_AeroDyn15_Polar_00.dat
+
+# Allocations:
+# Finn - IEA_15MW_RWT_Onshore.opt, IEA-15-240-RWT_AeroDyn15_blade.dat
+# Irene - IEA-15-240-RWT_AeroDyn15_Polar_00.dat
+# Hubert - Airfoils/IEA-15-240-RWT_AF00_Coords.txt
